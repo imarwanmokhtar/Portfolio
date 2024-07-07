e89467d99c58ad11d71a382aa766b59a4f284c96
imarwanmokhtar <marwanmokhtar000@gmail.com>
2024-07-07 12:56:06 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid"
+import RecentProjects from "@/components/RecentProjects";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
